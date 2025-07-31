54b0dd6f790480be7547b16b24c63430ffefe842
shaysingh818 <shalinsingh818@gmail.com>
2025-07-31 17:18:22 -0500

    add query

--- databases/data_exploration/sql/R__btc_usd_historical.sql
+++ databases/data_exploration/sql/R__btc_usd_historical.sql
@@ -8,3 +8,25 @@ CREATE TABLE IF NOT EXISTS BTC_US_HISTORICAL (
 )
 ENGINE = MergeTree
 ORDER BY Timestamp;
+
+CREATE OR REPLACE VIEW BTC_AVG_DAILY_PRICES AS
+SELECT 
+	toDate(Timestamp) as my_date,
+	count(my_date),
+	min(Open) as minOpen,
+	max(Open) as maxOpen,
+	avg(Open) as averageOpen,
+	min(High) as minHigh,
+	max(High) as maxHigh,
+	avg(High) as avgHigh,
+	min(Low) as minLow,
+	max(Low) as maxLow,
+	avg(Low) as avgLow,
+	min(Close) as minClose,
+	max(Close) as maxClose,
+	avg(Close) as avgClose,
+	min(Volume) as minVolume,
+	max(Volume)  as maxVolume,
+	avg(Volume) as avgVolume
+FROM BTC_US_HISTORICAL
+GROUP BY my_date;
