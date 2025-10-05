354e9016ffe95bc95f190d9289ecf788c77cb2b2
shaysingh818 <shalinsingh818.com>
2025-10-05 16:02:44 -0500

    add outputs

---
+++ modules/aws/networking/outputs.tf
@@ -0,0 +1,14 @@
+output "db_security_group_ids" {
+  description = "IDs of the security groups for the RDS instance"
+  value       = [aws_security_group.rds.id]
+}
+
+output "db_subnet_group_name" {
+  description = "Name of the DB subnet group"
+  value       = aws_db_subnet_group.propulsion.name
+}
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
